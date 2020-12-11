8bf7ace57a2d6caa1aaf87ad95dbe712dabcc0bb
RiandroProenca <RiandroProenca@users.noreply.github.com>
2020-12-11 18:47:46 -0300

    Initial project

---
+++ 4all/4all/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  4all
+//
+//  Created by Riandro Proença on 11/12/20.
+//  Copyright © 2020 Hub 4all. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
